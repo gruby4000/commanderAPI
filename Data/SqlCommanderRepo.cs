268490535b8609e8e5f28abcdb5866883b446382
Michal <michal.karpinski@procodice.pl>
2023-10-24 16:18:09 +0200

    sqlCommanderRepo,mockCommanderRepo,iCommanderRepo

---
+++ Data/SqlCommanderRepo.cs
@@ -0,0 +1,37 @@
+﻿using Commander.Models;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly CommanderContext _context;
+        public SqlCommanderRepo(CommanderContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command command)
+        {
+            if(command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+            _context.Add(command);
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.Commands.FirstOrDefault(command => command.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
